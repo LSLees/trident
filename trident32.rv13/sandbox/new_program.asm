4478bee8982f5600cacc35234a7577cfd68ee600
Louis Stockdale-Lees <lslees1346@gmail.com>
2026-07-14 00:56:01 +0100

    Branching instructions and layout overhaul

--- trident32.rv13/sandbox/new_program.asm
+++ trident32.rv13/sandbox/new_program.asm
@@ -1,1 +1,21 @@
-cmp r0, r0, r0
+movi r4, 0x8000
+movi r0, 48
+movi r1, 18
+
+loop:
+	cmp r0, r1
+	jeq done
+	sub r2, r0, r1
+	and r3, r2, r4
+	cmp r3, r4
+	jeq bigger
+
+	sub r0, r0, r1
+	jmp loop
+
+bigger:
+	sub r1, r1, r0
+	jmp loop
+
+done:
+	jmp done
